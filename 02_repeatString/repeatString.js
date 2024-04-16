5d5b1bb16e46b1f01c31485c1b90683d901a4668
itisover <143436308+itisover@users.noreply.github.com>
2024-04-16 20:06:58 +0100

    Finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let output_text = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for(num; num > 0; num--) {
+        output_text += text;
+    }
+    return output_text;
 };
 
 // Do not edit below this line
