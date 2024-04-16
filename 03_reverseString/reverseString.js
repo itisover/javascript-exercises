9a2d1a98388e4c5a6cdb65298c1683cb2a945880
itisover <143436308+itisover@users.noreply.github.com>
2024-04-16 20:10:36 +0100

    finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let buffer = "";
+    let i;
+    for(i = text.length - 1; i >= 0; i--) {
+        buffer += text[i];
+    }
+    return buffer;
 };
 
 // Do not edit below this line
